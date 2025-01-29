08d0fefb9b73748169de252d2e1a4e3328bcebf0
Jordan Mo <jordan@Jordans-MBP.lan>
2025-01-29 11:33:20 -0800

    Uploading solution to question 71 simplify path

---
+++ 71_Simplify_Path.py
@@ -0,0 +1,17 @@
+'''
+https://leetcode.com/problems/simplify-path/description/
+'''
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        path = path.split('/')
+        skip = set(['..', '.', '/', ''])
+        for c in path:
+            if c == '..' and stack:
+                stack.pop()
+            elif c not in skip:
+                stack.append(c)
+        res = ''
+        for c in stack:
+            res += '/' + c
+        return res if res != '' else '/'
