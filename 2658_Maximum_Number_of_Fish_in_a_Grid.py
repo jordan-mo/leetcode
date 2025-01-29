6686dc4be2ab13806b0561a4a9871a38f2f92fa2
Jordan Mo <jordan@Jordans-MBP.lan>
2025-01-29 11:36:35 -0800

    Uploading solution to question #2658

---
+++ 2658_Maximum_Number_of_Fish_in_a_Grid.py
@@ -0,0 +1,29 @@
+'''
+https://leetcode.com/problems/maximum-number-of-fish-in-a-grid/description/
+'''
+class Solution:
+    def findMaxFish(self, grid: List[List[int]]) -> int:
+        res = 0
+        n, m = len(grid), len(grid[0])
+        d = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        visited = set()
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] != 0 and (i, j) not in visited:
+                    #print(i,j)
+                    q = deque([(i, j)])
+                    tmp = 0
+                    while q:
+                        x, y = q.popleft()
+                        if (x, y) in visited:
+                            continue
+                        tmp += grid[x][y]
+                        for dx, dy in d:
+                            mx, my = dx+x, dy+y
+                            if 0 <= mx < n and 0 <= my < m:
+                                if grid[mx][my] != 0:
+                                    q.append((mx, my))
+                        visited.add((x, y))
+                    res = max(res, tmp)
+            
+        return res
