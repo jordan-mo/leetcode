d8adf016beae04426ad201cea2d37d805dfa4f9e
Jordan Mo <jordan@Jordans-MBP.lan>
2025-01-31 11:54:07 -0800

    Uploading problems 1492 and 2405

---
+++ 2405_Optimal_Partition_of_String.py
@@ -0,0 +1,15 @@
+'''
+Difficulty: Medium
+https://leetcode.com/problems/optimal-partition-of-string/description/
+'''
+
+class Solution:
+    def partitionString(self, s: str) -> int:
+        tmp = set()
+        res = 1
+        for c in s:
+            if c in tmp:
+                tmp = set()
+                res += 1
+            tmp.add(c)
+        return res
